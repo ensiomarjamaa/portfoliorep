11a2b20d683e3216f2a3c5e2376a8205a20f37ad
Anssi Soinio <91777480+ensiomarjamaa@users.noreply.github.com>
2021-10-09 13:22:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <h1>Hello</h1>
+<div class="php-test">
+
+  <?php
+  $str = "https://api.github.com/users/ensiomarjamaa";
+  echo $str
+  
+  ?>
+</div>
 <div id="rest-test">
   <script> 
       jQuery(function(){
